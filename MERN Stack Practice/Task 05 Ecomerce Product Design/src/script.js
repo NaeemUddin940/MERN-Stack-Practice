2e66efbd3de74df02176873f99f33822b6513cd8
NAEEM UDDIN <mdnaeemuddin14@gmail.com>
2025-03-14 15:29:04 +0600

    E-commerce product page Completed

--- MERN Stack Practice/Task 05 Ecomerce Product Design/src/script.js
+++ MERN Stack Practice/Task 05 Ecomerce Product Design/src/script.js
@@ -1,5 +1,13 @@
 const ListItem = document.getElementById('list')
 const cancel = document.querySelector(".cancel")
+const Cart = document.getElementById("Cart")
+const imgMain = document.querySelector(".imgMain")
+const number = document.querySelector(".number")
+
+function imgSwitch(e){
+    let value = e.target.src;
+    imgMain.setAttribute("src", value)
+}
 
 function filelist(){
     ListItem.style.display = "block" 
@@ -8,4 +16,20 @@ function filelist(){
 
 function cnacelList(){
     ListItem.style.display = "none"
+}
+
+function CartToggle(){
+    Cart.classList.toggle("cart")
+}
+
+let count = 0;
+function increase(){
+    count += 1;
+    number.innerHTML = count
+}
+
+function decrease(){
+    count -= 1;
+    number.innerHTML = count
+
 }
